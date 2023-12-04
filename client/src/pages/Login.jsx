ab3d6223323a65fe2ecb7a1bada199394990482b
MAT-2 <mpcasey30@gmail.com>
2023-12-04 13:49:02 -0600

    Added basic start for developing login card.

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import Card from "react-bootstrap/Card";
+
+function Login() {
+  return (
+    <Card style={{ width: "25rem" }}>
+      <Card.Body></Card.Body>
+    </Card>
+  );
+}
+
+export default Login();
