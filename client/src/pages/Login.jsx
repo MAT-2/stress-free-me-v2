d1ebe28dc3f7a71301476567c5a9577d98ac8d5d
MAT-2 <mpcasey30@gmail.com>
2023-12-04 13:57:09 -0600

    Added login and card styling to Login.jsx. Added references.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,43 @@
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 function Login() {
   return (
     <Card style={{ width: "25rem" }}>
-      <Card.Body></Card.Body>
+      <Card.Body>
+        <Form>
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="email" placeholder="hello123@example.com" />
+            </Col>
+          </Form.Group>
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+          <Button variant="secondary" size="lg">
+            Login
+          </Button>
+        </Form>
+      </Card.Body>
     </Card>
   );
 }
