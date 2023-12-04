cf17ccf8923f508128e5fcb88981df344358fc56
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-12-04 13:45:39 -0600

    resolvers in process

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    thoughtAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
