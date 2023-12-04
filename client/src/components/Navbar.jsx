7203fd8fda94ee132836abc7f268639a94b13607
MAT-2 <mpcasey30@gmail.com>
2023-12-04 13:19:56 -0600

    Added navbar styling and linking between pages.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,53 @@
-import { Link, useLocation } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
   const homePage = useLocation().pathname;
 
-  return ();
+  return (
+    <Nav className="justify-content-end" activeKey="/home">
+      <Nav.Item>
+        <Nav.Link
+          href="/"
+          className={homePage === "/" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="/Login"
+          className={homePage === "/Login" ? "nav-link active" : "nav-link"}
+        >
+          Login
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="/Profile"
+          className={homePage === "/Profile" ? "nav-link active" : "nav-link"}
+        >
+          Profile
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="/Survey"
+          className={homePage === "/Survey" ? "nav-link active" : "nav-link"}
+        >
+          Survey
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="/Results"
+          className={homePage === "/Results" ? "nav-link active" : "nav-link"}
+        >
+          Results
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
 }
 
 export default Navbar();
