115dc5f5d04bd19f39e65c4c05309da542ac384c
MAT-2 <mpcasey30@gmail.com>
2023-12-04 11:17:24 -0600

    Added MongoDB connection string.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb database');
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://root:admin123@cluster0.l7purnz.mongodb.net/StressFreeMe"
+);
 
 module.exports = mongoose.connection;
