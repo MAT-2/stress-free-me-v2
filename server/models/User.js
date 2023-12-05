dc4b7858a7e81c802d9c33befa4717f174f8e72b
Diego Cornejo <cornejodl13@icloud.com>
2023-12-04 18:25:54 -0600

    User.js

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const user = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    }
+});
+
+user.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const rounds = 15;
+        this.password = await bcrypt.hash(this.password, rounds);
+    }
+
+    next();1
+});
+
+user.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+const User = model('User', user);
+
+module.exports = User;
