360c29013a532f51f35d3185185eb584183bcf06
Diego Cornejo <cornejodl13@icloud.com>
2023-12-07 14:36:14 -0600

    login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,40 +3,86 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth'
 
-function Login() {
+
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({email: '', password: ''});
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: '',
+      password: '',
+    });
+  }
+
   return (
     <div>
       <Card style={{ width: "25rem" }}>
-        <Card.Body>
-          <Card.Title>Login</Card.Title>
-          <Form>
-            <Form.Group as={Row} className="mb-3" controlId="emailForm">
-              <Form.Label column sm="2">
-                Email
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="email" placeholder="hello123@example.com" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3" controlId="passwordForm">
-              <Form.Label column sm="2">
-                Password
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="password" placeholder="Password" />
-              </Col>
-            </Form.Group>
-            <Button variant="secondary" size="lg">
-              Login
-            </Button>
-          </Form>
-          <div className="form-group mt-2">
-            <p>
-              <a href="#">New User? Signup here!</a>
-            </p>
-          </div>
-        </Card.Body>
+        {data ? (
+          <p>
+          Success! You may now head{' '}
+          <Link to="/Profile">to the profile page.</Link>
+        </p>
+        ) :(
+            <Card.Body>
+            <Card.Title>Login</Card.Title>
+            <Form onSubmit={handleFormSubmit}>
+              <Form.Group as={Row} className="mb-3" controlId="emailForm">
+                <Form.Label column sm="2">
+                  Email
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control placeholder="hello123@example.com" type="email" name="email" value={formState.email} onChange={handleChange}/>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="passwordForm">
+                <Form.Label column sm="2">
+                  Password
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control placeholder="Password" type="password" name="password" value={formState.password} onChange={handleChange}/>
+                </Col>
+              </Form.Group>
+              <Button  type="submit" variant="secondary" size="lg">
+                Login
+              </Button>
+            </Form>
+            <div className="form-group mt-2">
+              <p>
+                <a href="#">New User? Signup here!</a>
+              </p>
+            </div>
+          </Card.Body>
+        )}
+        
       </Card>
 
       <Card style={{ width: "25rem" }}>
