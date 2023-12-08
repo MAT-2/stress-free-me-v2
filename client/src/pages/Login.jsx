f54be9208700f44c2dd57c3e9782a827a9934a3e
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-12-08 12:06:03 -0600

    Working on login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,12 +8,14 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth'
+import { redirect } from "react-router-dom"
 
 
 
 const Login = (props) => {
   const [formState, setFormState] = useState({email: '', password: ''});
   const [login, { error, data }] = useMutation(LOGIN_USER);
+
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -31,8 +33,11 @@ const Login = (props) => {
       const { data } = await login({
         variables: { ...formState },
       });
-
-      Auth.login(data.login.token);
+if (data) {
+  Auth.login(data.login.token)
+  return redirect ('/Profile')
+}
+      // Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
@@ -49,7 +54,7 @@ const Login = (props) => {
         {data ? (
           <p>
           Success! You may now head{' '}
-          <Link to="/Profile">to the profile page.</Link>
+          {/* <Link to="/Profile">to the profile page.</Link> */}
         </p>
         ) :(
             <Card.Body>
