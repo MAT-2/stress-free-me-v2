1afe10c37e76adb8d24fb14d0c84ce3d4a8ab098
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-12-08 19:39:33 -0600

    Sruvey routes to results page once the survey is finished

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
+// process.env.DB_NAME 
 
-
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_NAME );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.vw25nz0.mongodb.net/StressFreeMe' );
 
 module.exports = mongoose.connection;
