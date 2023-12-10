22a0c72c0457b78832dfaf76e94b9a7878d9acc4
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-12-10 16:42:51 -0600

    Update delete thoughts

--- client/src/components/ThoughtLIst/index.jsx
+++ client/src/components/ThoughtLIst/index.jsx
@@ -6,38 +6,40 @@ const ThoughtList = ({ thoughts = [] }) => {
     return <h1>There Are No Thoughts Existing</h1>;
   }
   const removeThought = (id) => {
-    const updatedThought = [...mythoughts].filter((item) => item.id !== id);
+    const updatedThought = mythoughts.filter((item) => item._id !== id);
     setMyThought(updatedThought);
   };
 
   // Function to edit the bucket list item
   const editThought = (itemId, newValue) => {
     // Make sure that the value isn't empty
-    if (!newValue.text) {
+    if (!newValue) {
       return;
     }
-
-    // We use the "prev" argument provided with the useState hook to map through our list of items
-    // We then check to see if the item ID matches the if of the item that was clicked and if so we set it to a new value
-    setMyThought((prev) =>
-      prev.map((item) => (item.id === itemId ? newValue : item))
+  
+    // Create a new array with the updated thought
+    const updatedThoughts = mythoughts.map((item) =>
+      item._id === itemId ? { ...item, thoughtText: newValue } : item
     );
+  
+    setMyThought(updatedThoughts);
   };
   return (
     <>
       <h2>Thought List</h2>
       <div>
-        {mythoughts &&
-          mythoughts.map((thought) => (
-            <div key={thought._id}>
-              <div>
-                <h5>Created on {thought.createdAt}</h5>
-                <h5>{thought.thoughtText}</h5>
-                <button onClick={removeThought(thought._id)}>Delete</button>
-                <button onClick={editThought(thought._id,"undefined" )}>Update</button>
-              </div>
+        {mythoughts.map((thought) => (
+          <div key={thought._id}>
+            <div>
+              <h5>Created on {thought.createdAt}</h5>
+              <h5>{thought.thoughtText}</h5>
+              <button onClick={() => removeThought(thought._id)}>Delete</button>
+              <button onClick={() => editThought(thought._id, "new text in here")}>
+                Update
+              </button>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </>
   );
